327948ac575ca16cf98f93d51baee0e55d03a0f4
ZeroLend <hello@zerolend.xyz>
2023-08-02 00:06:39 +0300

    initial commit

---
+++ deploy/deploy.ts
@@ -0,0 +1,65 @@
+import { Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// load env file
+import dotenv from "dotenv";
+dotenv.config();
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+
+if (!PRIVATE_KEY)
+  throw "⛔️ Private key not detected! Add it to the .env file!";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the pyth aggregator contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+
+  const artifact = await deployer.loadArtifact("EarlyZerolend");
+
+  // Estimate contract deployment fee
+  const args: any[] = [300];
+
+  const deploymentFee = await deployer.estimateDeployFee(artifact, args);
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const greeterContract = await deployer.deploy(artifact, args);
+
+  //obtain the Constructor Arguments
+  console.log(
+    "Constructor args:" + greeterContract.interface.encodeDeploy(args)
+  );
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+
+  // verify contract for tesnet & mainnet
+  if (process.env.NODE_ENV != "test") {
+    // Contract MUST be fully qualified name (e.g. path/sourceName:contractName)
+    const contractFullyQualifedName =
+      "contracts/EarlyZerolend.sol:EarlyZerolend";
+
+    // Verify contract programmatically
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      contract: contractFullyQualifedName,
+      constructorArguments: args,
+      bytecode: artifact.bytecode,
+    });
+  } else {
+    console.log(`Contract not verified, deployed locally.`);
+  }
+}
